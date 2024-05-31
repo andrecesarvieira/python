9bf48220901ac671f958efe7a02ec2591d9f3968
André César Vieira <andrecesarvieira@hotmail.com>
2024-05-30 23:53:43 -0300

    Update

--- Tkinter/frmMain.py
+++ Tkinter/frmMain.py
@@ -10,20 +10,25 @@ from tkinter import ttk, TclError
 def criar_frame(root):
 
     # Definindo grid para alocar os widgets
-    frm_main = ttk.Frame(root)
-    frm_main.columnconfigure(0, weight=1)
-    frm_main.columnconfigure(0, weight=3)
+    frame = ttk.Frame(root)
+    frame.columnconfigure(0, weight=1)
+    frame.columnconfigure(0, weight=3)
 
     # Alocando widgets no grid
-    ttk.Label(master=frm_main, text="Usuário:").grid(column=0, row=0, sticky=W)
-    usuario = ttk.Entry(frm_main, width=30)
+    ttk.Label(master=frame, text="Usuário:").grid(column=0, row=0, sticky=W)
+    usuario = ttk.Entry(frame, width=30)
     usuario.focus()
     usuario.grid(column=1, row=0, sticky=W)
 
-    ttk.Label(master=frm_main, text="Senha:").grid(column=0, row=1, sticky=W)
-    usuario = ttk.Entry(frm_main, width=30)
+    ttk.Label(master=frame, text="Senha:").grid(column=0, row=1, sticky=W)
+    usuario = ttk.Entry(frame, width=30)
     usuario.focus()
     usuario.grid(column=1, row=1, sticky=W)
+
+    for widget in frame.winfo_children():
+        widget.grid(padx=5, pady=5)
+
+    return frame
 
 
 def criar_janela_principal():
@@ -33,15 +38,8 @@ def criar_janela_principal():
 
     # Atribuindo valores padrões da janela
     root.title(string="Login")
-    root.config(background="LightGray")
     root.iconphoto(False, PhotoImage(file="logo.png"))
     root.resizable(height=False, width=False)
-
-    # Remover os botões minimizar e maximizar (somente sistema Windows)
-    try:
-        root.attributes('-toolwindow', True)
-    except TclError:
-        print('Plataforma não suportada')
 
     root_altura = 300
     root_largura = 400
@@ -59,10 +57,10 @@ def criar_janela_principal():
     root.columnconfigure(0, weight=4)
     root.columnconfigure(1, weight=1)
 
-    root.mainloop()
+    frame = criar_frame(root)
+    frame.grid(column=0, row=0)
 
-    input_frame = criar_frame(root)
-    input_frame.grid(column=0, row=0)
+    root.mainloop()
 
 
 if __name__ == "__main__":
