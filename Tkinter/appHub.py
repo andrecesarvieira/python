02e4d99039f25d7abe4e0f37ac92715e60a76b40
André César Vieira <andrecesarvieira@hotmail.com>
2024-06-01 01:50:30 -0300

    Update

--- Tkinter/appHub.py
+++ Tkinter/appHub.py
@@ -43,24 +43,24 @@ class AppHub:
         self.root.mainloop()
 
     def criar_widgets_menu(self):
+
         # Imagens
-        self.toggle_icone = tk.PhotoImage(file="images/menu.png")
-        self.agenda_icone = tk.PhotoImage(file="images/menu_agenda.png")
-        self.cadastro_icone = tk.PhotoImage(file="images/menu_cadastro.png")
-        self.financeiro_icone = tk.PhotoImage(
-            file="images/menu_financeiro.png")
-        self.pedidos_icone = tk.PhotoImage(file="images/menu_pedidos.png")
-        # produtos_icone = tk.PhotoImage(file="images/menu_produtos.png")
-        # config_icone = tk.PhotoImage(file="images/menu_config.png")
-        # sair_icone = tk.PhotoImage(file="images/menu_sair.png")
+        self.toggle_png = tk.PhotoImage(file="images/menu.png")
+        self.agenda_png = tk.PhotoImage(file="images/menu_agenda.png")
+        self.cadastro_png = tk.PhotoImage(file="images/menu_cadastro.png")
+        self.financeiro_png = tk.PhotoImage(file="images/menu_financeiro.png")
+        self.pedidos_png = tk.PhotoImage(file="images/menu_pedidos.png")
+        self.produtos_png = tk.PhotoImage(file="images/menu_produtos.png")
+        self.config_png = tk.PhotoImage(file="images/menu_config.png")
+        self.sair_png = tk.PhotoImage(file="images/menu_sair.png")
 
         # Botão Toggle
-        botao_toggle = tk.Button(self.menu_frame, image=self.toggle_icone, background=self.menu_frame_cor,
+        botao_toggle = tk.Button(self.menu_frame, image=self.toggle_png, background=self.menu_frame_cor,
                                  border=0, activebackground=self.menu_frame_cor)
         botao_toggle.place(x=4, y=10)
 
         # Botão Agenda
-        botao_agenda = tk.Button(self.menu_frame, image=self.agenda_icone, background=self.menu_frame_cor,
+        botao_agenda = tk.Button(self.menu_frame, image=self.agenda_png, background=self.menu_frame_cor,
                                  border=0, activebackground=self.menu_frame_cor,
                                  command=lambda: self.botoes_menu(ind=botao_agenda_ind, pagina=self.agenda))
         botao_agenda.place(x=8, y=113, width=32, height=35)
@@ -68,30 +68,27 @@ class AppHub:
         botao_agenda_ind.place(x=3, y=113, width=2, height=35)
 
         # Botão Cadastro
-        botao_cadastro = tk.Button(self.menu_frame, image=self.cadastro_icone, background=self.menu_frame_cor,
+        botao_cadastro = tk.Button(self.menu_frame, image=self.cadastro_png, background=self.menu_frame_cor,
                                    border=0, activebackground=self.menu_frame_cor,
                                    command=lambda: self.botoes_menu(ind=botao_cadastro_ind, pagina=self.cadastro))
         botao_cadastro.place(x=8, y=160, width=30, height=35)
-        botao_cadastro_ind = tk.Label(
-            self.menu_frame, background=self.menu_frame_cor)
+        botao_cadastro_ind = tk.Label(self.menu_frame, background=self.menu_frame_cor)
         botao_cadastro_ind.place(x=3, y=160, width=2, height=35)
 
         # Botão Financeiro
-        botao_financeiro = tk.Button(self.menu_frame, image=self.financeiro_icone, background=self.menu_frame_cor,
+        botao_financeiro = tk.Button(self.menu_frame, image=self.financeiro_png, background=self.menu_frame_cor,
                                      border=0, activebackground=self.menu_frame_cor,
                                      command=lambda: self.botoes_menu(ind=botao_financeiro_ind, pagina=self.financeiro))
         botao_financeiro.place(x=8, y=210, width=30, height=35)
-        botao_financeiro_ind = tk.Label(
-            self.menu_frame, background=self.menu_frame_cor)
+        botao_financeiro_ind = tk.Label(self.menu_frame, background=self.menu_frame_cor)
         botao_financeiro_ind.place(x=3, y=210, width=2, height=35)
 
         # Botão Pedidos
-        botao_pedidos = tk.Button(self.menu_frame, image=self.pedidos_icone, background=self.menu_frame_cor,
+        botao_pedidos = tk.Button(self.menu_frame, image=self.pedidos_png, background=self.menu_frame_cor,
                                   border=0, activebackground=self.menu_frame_cor,
                                   command=lambda: self.botoes_menu(ind=botao_pedidos_ind, pagina=self.pedidos))
         botao_pedidos.place(x=8, y=260, width=30, height=35)
-        botao_pedidos_ind = tk.Label(
-            self.menu_frame, background=self.menu_frame_cor)
+        botao_pedidos_ind = tk.Label(self.menu_frame, background=self.menu_frame_cor)
         botao_pedidos_ind.place(x=3, y=260, width=2, height=35)
 
     def botoes_menu(self, ind, pagina):
@@ -108,8 +105,7 @@ class AppHub:
     def agenda(self):
         frame_agenda = tk.Frame(self.main_frame)
 
-        lbl_texto = tk.Label(
-            frame_agenda, text="Página Agenda\n\n1", font="Arial 30 bold")
+        lbl_texto = tk.Label(frame_agenda, text="Página Agenda\n\n1", font="Arial 30 bold")
         lbl_texto.pack()
 
         frame_agenda.pack()
@@ -117,8 +113,7 @@ class AppHub:
     def cadastro(self):
         frame_cadastro = tk.Frame(self.main_frame)
 
-        lbl_texto = tk.Label(
-            frame_cadastro, text="Página Cadastro\n\n2", font="Arial 30 bold")
+        lbl_texto = tk.Label(frame_cadastro, text="Página Cadastro\n\n2", font="Arial 30 bold")
         lbl_texto.pack()
 
         frame_cadastro.pack()
@@ -126,8 +121,7 @@ class AppHub:
     def financeiro(self):
         frame_financeiro = tk.Frame(self.main_frame)
 
-        lbl_texto = tk.Label(
-            frame_financeiro, text="Página Financeiro\n\n3", font="Arial 30 bold")
+        lbl_texto = tk.Label(frame_financeiro, text="Página Financeiro\n\n3", font="Arial 30 bold")
         lbl_texto.pack()
 
         frame_financeiro.pack()
@@ -135,8 +129,7 @@ class AppHub:
     def pedidos(self):
         frame_pedidos = tk.Frame(self.main_frame)
 
-        lbl_texto = tk.Label(
-            frame_pedidos, text="Página Pedidos\n\n4", font="Arial 30 bold")
+        lbl_texto = tk.Label(frame_pedidos, text="Página Pedidos\n\n4", font="Arial 30 bold")
         lbl_texto.pack()
 
         frame_pedidos.pack()
