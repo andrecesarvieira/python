0198d36f753a29381a2e400560fcd731d4e8c570
André César Vieira <andrecesarvieira@hotmail.com>
2024-06-03 23:09:46 -0300

    Update

--- Gerenciamento de Cursos e Alunos/modulos/db/db_tabela_turmas.py
+++ Gerenciamento de Cursos e Alunos/modulos/db/db_tabela_turmas.py
@@ -4,24 +4,62 @@
 # Data....: 2/6/24
 
 import os
-import sys
 import sqlite3
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 class CrudTabelaTurmas():
-  pass
+  local = f"(" + __qualname__ + " -> " + os.path.basename(__file__) + ")"
+
+  def inserir(self, con, dados):
+    try:
+      with con:
+        cur = con.cursor()
+        query = """INSERT INTO TURMAS (nome, curso, data_inicio)
+                   VALUES (?, ?, ?)"""
+        cur.execute(query, dados)
+    except sqlite3.Error as erro:
+      print (f"Erro ao inserir na tabela TURMAS: {erro}, {self.local}")
+
+  def ler(self, con) -> list: 
+    try:
+      with con:
+        tabela = []
+        cur = con.cursor()
+        cur.execute("SELECT * FROM TURMAS")
+        res = cur.fetchall()
+        for i in res:
+          tabela.append(i)
+    except sqlite3.Error as erro:
+      print (f"Erro ao ler a tabela TURMAS: {erro}, {self.local}")
+    else:
+      return tabela
+  
+  def atualizar(self, con, dados):
+    try:
+      with con:
+        cur = con.cursor()
+        query = """UPDATE TURMAS
+                   SET nome=?, curso=?, data_inicio=?
+                   WHERE id=?"""
+        cur.execute(query, dados)
+    except sqlite3.Error as erro:
+      print (f"Erro ao atualizar na tabela TURMAS: {erro}, {self.local}")
+
+  def deletar(self, con, id):
+      try:
+        with con:          
+          cur = con.cursor()
+          query = "DELETE FROM TURMAS WHERE id=?"
+          cur.execute(query, (id,))
+      except sqlite3.Error as erro:
+          print(f"Erro ao deletar na tabela TURMAS: {erro}, {self.local}")
 
 class CriarTabelaTurmas():
-  
   local = f"(" + __qualname__ + " -> " + os.path.basename(__file__) + ")"  
 
   def criar(self, con):
-
-    self.con = con
-
     try:
-      with self.con:
-        cur = self.con.cursor()
+      with con:
+        cur = con.cursor()
         cur.execute(""" CREATE TABLE IF NOT EXISTS TURMAS(
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     nome TEXT,
@@ -30,6 +68,6 @@ class CriarTabelaTurmas():
                     FOREIGN KEY (curso) REFERENCES CURSOS (nome)
                     ON UPDATE CASCADE ON DELETE CASCADE)""")
     except sqlite3.Error as erro:
-      print ("Erro ao criar tabela TURMAS: ", erro, self.local)
+      print (f"Erro ao criar tabela TURMAS: {erro}, {self.local}")
     else:
       print("Tabela TURMAS criada.")
