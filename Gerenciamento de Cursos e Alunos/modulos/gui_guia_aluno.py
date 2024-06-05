dc4c2aa9f753aca0aceefd6624f3f7bc3da26340
André César Vieira <andrecesarvieira@hotmail.com>
2024-06-05 17:33:58 -0300

    Em desenvolvimento

--- Gerenciamento de Cursos e Alunos/modulos/gui_guia_aluno.py
+++ Gerenciamento de Cursos e Alunos/modulos/gui_guia_aluno.py
@@ -4,34 +4,35 @@
 
 from tkinter import ttk
 
+
 class AbaAlunos():
-  def __init__(self, guia_alunos, style):
-    self.guia_alunos = guia_alunos
-    self.style = style
+    def __init__(self, guia_alunos, style):
+        self.guia_alunos = guia_alunos
+        self.style = style
 
-    for widgets in self.guia_alunos.winfo_children():
-      widgets.destroy()
+        for widgets in self.guia_alunos.winfo_children():
+            widgets.destroy()
 
-    self.criar_frames()
-    #self.criar_widgets()
+        self.criar_frames()
+        # self.criar_widgets()
 
-  def criar_frames(self):
-    
-    #Para teste
-    self.style.configure('Frame1.TFrame', background='red')
-    self.style.configure('Frame2.TFrame', background='blue')
-    self.style.configure('Frame3.TFrame', background='green')
-    
-    # Criar frames na guia alunos
-    self.frame_menu = ttk.Frame(self.guia_alunos, width=1272, height=40, style='Frame1.TFrame',
-                                relief="raised", border=0, borderwidth=0)
-    self.frame_menu.grid(row=1, column=0, padx=3, pady=0, sticky='nsew')
-    self.frame_corpo = ttk.Frame(self.guia_alunos, width=1272, height=280, style='Frame2.TFrame')
-    self.frame_corpo.grid(row=2, column=0, padx=3, pady=0, sticky='nsew')
-    self.frame_grade = ttk.Frame(self.guia_alunos, width=1272, height=280, style='Frame3.TFrame',
-                                 relief='raised', border=0, borderwidth=0)
-    self.frame_grade.grid(row=3, column=0, padx=3, pady=0, sticky='nsew')
+    def criar_frames(self):
 
-  def criar_widgets(self):
-    pass
+        # Para teste
+        self.style.configure('Frame1.TFrame', background='red')
+        self.style.configure('Frame2.TFrame', background='blue')
+        self.style.configure('Frame3.TFrame', background='green')
 
+        # Criar frames na guia alunos
+        self.frame_menu = ttk.Frame(self.guia_alunos, width=1272, height=40, style='Frame1.TFrame',
+                                    relief="raised", border=0, borderwidth=0)
+        self.frame_menu.grid(row=1, column=0, padx=0, pady=0, sticky='nsew')
+        self.frame_corpo = ttk.Frame(
+            self.guia_alunos, width=1272, height=280, style='Frame2.TFrame')
+        self.frame_corpo.grid(row=2, column=0, padx=0, pady=0, sticky='nsew')
+        self.frame_grade = ttk.Frame(self.guia_alunos, width=1272, height=280, style='Frame3.TFrame',
+                                     relief='raised', border=0, borderwidth=0)
+        self.frame_grade.grid(row=3, column=0, padx=0, pady=0, sticky='nsew')
+
+    def criar_widgets(self):
+        pass
--- Gerenciamento de Cursos e Alunos/modulos/gui_janela_principal.py
+++ Gerenciamento de Cursos e Alunos/modulos/gui_janela_principal.py
@@ -4,95 +4,99 @@
 
 import tkinter as tk
 
+from datetime import datetime
 from tkinter import ttk
 from tkinter import font
 from PIL import Image, ImageTk
 
 from modulos.gui_guia_aluno import AbaAlunos
-#from modulos.guia_cursos_criar import GuiaCursos -> à implementar
-#from modulos.guia_turmas_criar import GuiaTurmas -> à implementar
+# from modulos.guia_cursos_criar import GuiaCursos -> à implementar
+# from modulos.guia_turmas_criar import GuiaTurmas -> à implementar
 
 class JanelaPrincipal():
-  def __init__(self, root):
-    self.root = root
-    self.root.title('Gerenciamento de Escola')
-    self.root.resizable(False, False)
+    def __init__(self, root):
+        self.root = root
+        self.root.title('Gerenciamento de Escola')
+        self.root.resizable(False, False)
 
-    # Centralizar o início da aplicação, considerando a resolução do sistema
-    LARGURA = 1280
-    ALTURA = 720
-    MENU_DO_WINDOWS = 80
-    tela_largura = self.root.winfo_screenwidth()
-    tela_altura = self.root.winfo_screenheight() - MENU_DO_WINDOWS
-    x_coordenada = int((tela_largura / 2) - (LARGURA / 2))
-    y_coordenada = int((tela_altura / 2) - (ALTURA / 2))
-    self.root.geometry(f'{LARGURA}x{ALTURA}+{x_coordenada}+{y_coordenada}')
+        # Centralizar o início da aplicação, considerando a resolução do sistema
+        LARGURA = 1280
+        ALTURA = 720
+        MENU_DO_WINDOWS = 80
+        tela_largura = self.root.winfo_screenwidth()
+        tela_altura = self.root.winfo_screenheight() - MENU_DO_WINDOWS
+        x_coordenada = int((tela_largura / 2) - (LARGURA / 2))
+        y_coordenada = int((tela_altura / 2) - (ALTURA / 2))
+        self.root.geometry(f'{LARGURA}x{ALTURA}+{x_coordenada}+{y_coordenada}')
 
-    self.criar_frames()
-    self.criar_widgets()
+        # Definir cores e fontes
+        self.COR_FUNDO_LOGO = '#0E2B4A'
+        self.COR_FONTE_LOGO = '#FFFFFF'
+        self.FAM_FONTE_LOGO = font.Font(family='JetBrains Mono ExtraBold', name='logo', size=18, weight='normal')
 
-  def criar_frames(self):
-    self.style = ttk.Style(self.root)  
+        self.COR_FONTE_PADRAO = '#0E2B4A'
+        self.FAM_FONTE_PADRAO = font.Font(family='Arial', name='padrao', size=8, weight='bold')
+
+        self.criar_frames()
+        self.criar_widgets()
     
-    # Criar frames logo e menu -> em implementação (cor de fundo do frame_logo não está funcionando)
-    self.style.configure('custom.TFrame', background='green', relief='sunken')    
-    self.frame_topo = ttk.Frame(self.root, width=1280, height=60, relief='raised',style='custom.TFrame')
-    self.frame_topo.config(borderwidth=1)
-    self.frame_topo.grid(row=0, column=0, columnspan=1, pady=2, padx=1, sticky='nsew')
-    
-    self.frame_meio = ttk.Frame(self.root, width=1280, height=640, style='custom.TFrame')
-    self.frame_meio.grid(row=2, column=0, columnspan=1, pady=0, padx=1, sticky='nsew')
-    
-    self.frame_rodape = ttk.Frame(self.root, width=1276, height=20, style='Teste.TFrame', relief='flat')
-    self.frame_rodape.grid(row=3, column=0, columnspan=1, pady=0, padx=1, sticky='s')
+    def criar_frames(self):
+        self.style = ttk.Style(self.root)
 
-  def criar_widgets(self):
-    # Definir cores e fonte
-    COR_FONTE = '#0E2B4A'
-    FONTE_LOGO = font.Font(family='JetBrains Mono ExtraBold', name='logo', size=18, weight='normal')
+        # Criar frames logo e menu -> em implementação (cor de fundo do frame_logo não está funcionando)
+        self.style.configure('Logo.TFrame', background = self.COR_FUNDO_LOGO)
+        self.frame_topo = ttk.Frame(self.root, width=1278, height=50, style='Logo.TFrame')
+        self.frame_topo.config(borderwidth=2, relief='groove')
+        self.frame_topo.grid(row=0, padx=1, sticky='nsew')
 
-    # Definir icone e descrição no frame_logo
-    o = Image.open(fp='imagens/img_alunos.png')
-    r = o.resize(size=(40,40))
-    img = ImageTk.PhotoImage(r)
-    
-    lbl_logo_img = ttk.Label(self.frame_topo, image=img, width=60,
-                            compound='left', anchor='nw')
-    lbl_logo_img.image = img
-    lbl_logo_img.place(x=340, y=7)
-    lbl_logo_txt = ttk.Label(self.frame_topo, text='Escola de Desenvolvimento Python', width=60,
-                            compound='left', anchor='w',
-                            font=FONTE_LOGO, foreground=COR_FONTE)
-    lbl_logo_txt.place(x=400, y=12)
+        self.frame_meio = ttk.Frame(self.root, width=1278, height=644)
+        self.frame_meio.config(borderwidth=2, relief='groove')
+        self.frame_meio.grid(row=1, padx=1, sticky='nsew')
 
-    # Definir tema styles para as abas (bootstrap)
-    self.style.theme_create(
-        'padrao', parent='alt', settings={
-        'TNotebook':     {'configure': {'tabmargins': [2, 5, 2, 0],
-                                        'borderwidth': (0)} },
-        'TNotebook.Tab': {'configure': {'padding': [20, 3],
-                                        'font': ('JetBrains Mono Bold', 11),
-                                        'borderwidth': (2)},
-        'map':           {'foreground': [('selected', COR_FONTE)],
-        'expand':        [('selected', [0, 0, 0, 0])] } } } )
-    
-    self.style.theme_use('padrao')
-    
-    # Criação das abas (bootstrap) no frame_abas
-    Abas = ttk.Notebook(self.frame_meio)
-    aba_alunos = ttk.Frame(Abas)
-    aba_cursos = ttk.Frame(Abas)
-    aba_turmas = ttk.Frame(Abas)
-    Abas.add(aba_alunos, text='Alunos')
-    Abas.add(aba_cursos, text='Cursos')
-    Abas.add(aba_turmas, text='Turmas')
-    Abas.pack(expand = 1, fill ='both') 
+        self.frame_rodape = ttk.Frame(self.root, width=1278, height=25)
+        self.frame_rodape.config(borderwidth=2, relief='flat')
+        self.frame_rodape.grid(row=2, padx=1, sticky='nsew')
 
-    # Criar label de mensagem para o usuário no frame_msg
-    lbl_msg_txt = ttk.Label(self.frame_rodape, width=40,anchor='w', text="Sem mensagens.")
-    lbl_msg_txt.place(x=0, y=0)
+    def criar_widgets(self):
+        # Definir icone e descrição no frame_logo
+        o = Image.open(fp='imagens/img_alunos.png')
+        r = o.resize(size=(35, 35))
+        img = ImageTk.PhotoImage(r)
 
-    # Criação dos widgets alunos nas abas
-    alunos = AbaAlunos(aba_alunos, self.style)
-    #cursos = CriarFramesAlunos(aba_cursos) -> à implementar
-    #turmas = CriarFramesAlunos(aba_turmas) -> à implementar
+        lbl_logo_img = ttk.Label(self.frame_topo, image=img, compound='left', anchor='nw',
+                                 background=self.COR_FUNDO_LOGO)
+        lbl_logo_img.image = img
+        lbl_logo_img.place(x=340, y=4)
+        lbl_logo_txt = ttk.Label(self.frame_topo, text='Escola de Desenvolvimento Python', width=32,
+                                 compound='left', anchor='w', font=self.FAM_FONTE_LOGO,
+                                 foreground=self.COR_FONTE_LOGO, background=self.COR_FUNDO_LOGO)
+        lbl_logo_txt.place(x=400, y=6)
+
+        # Criação das abas (bootstrap) no frame_abas
+        #Abas = ttk.Notebook(self.frame_meio)
+        #aba_alunos = ttk.Frame(Abas)
+        #aba_cursos = ttk.Frame(Abas)
+        #aba_turmas = ttk.Frame(Abas)
+        #Abas.add(aba_alunos, text='Alunos')
+        #Abas.add(aba_cursos, text='Cursos')
+        #Abas.add(aba_turmas, text='Turmas')
+        #Abas.pack(expand=1, fill='both')
+
+        # Criar label de mensagem para o usuário no frame_msg
+        o = Image.open(fp='imagens/notificacao.png')
+        r = o.resize(size=(16, 14))
+        img2 = ImageTk.PhotoImage(r)
+        
+        lbl_msg_img = ttk.Label(self.frame_rodape, image=img2, anchor='w')
+        lbl_msg_img.image = img2
+        lbl_msg_img.place(x=0, y=0)
+
+        hora = datetime.now()
+        lbl_msg_txt = ttk.Label(self.frame_rodape, text=hora.strftime('%H:%M:%S') + ' - Sistema funcionando corretamente',
+                                anchor='w', font=self.FAM_FONTE_PADRAO)
+        lbl_msg_txt.place(x=20, y=0)
+
+        # Criação dos widgets alunos nas abas
+        # alunos = AbaAlunos(aba_alunos, self.style)
+        # cursos = CriarFramesAlunos(aba_cursos) -> à implementar
+        # turmas = CriarFramesAlunos(aba_turmas) -> à implementar
